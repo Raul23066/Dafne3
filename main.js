87b19f5cd0fef0b81aad2e8ea53fa8fcf822cd0b
Raul23066 <140854151+Raul23066@users.noreply.github.com>
2024-02-10 02:30:00 -0600

    Update main.js

--- main.js
+++ main.js
@@ -124,7 +124,7 @@ const connectionOptions = {
         logger: pino({ level: 'silent' }),
         printQRInTerminal: !pairingCode, 
         mobile: useMobile, 
-        browser: ['Larios.Psd (Linux)', '', ''],
+        browser: ['DAFNESITA BOT (Linux)', '', ''],
         auth: {
             creds: state.creds,
             keys: makeCacheableSignalKeyStore(state.keys, Pino({ level: "fatal" }).child({ level: "fatal" })),
